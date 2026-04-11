791e01b892eed219dea22ad39ea2fa0061c56954
AmrutaMohite28 <amrutamohite016@gmail.com>
2026-04-11 19:13:05 +0530

    Update bk.py

--- backend/bk.py
+++ backend/bk.py
@@ -266,8 +266,9 @@ def get_asl_letters():
     return jsonify({'letters': list(asl_recognizer.asl_patterns.keys())}), 200
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
-
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, debug=False, host='0.0.0.0', port=port)
+    
 import threading, requests, time
 
 def keep_alive():
